d52569452eedd324aa1f3afcb8534a83b5538e70
Tenoch <tesparza@gmail.com>
2016-10-29 23:51:45 -0400

    update

--- index.js
+++ index.js
@@ -4,25 +4,35 @@
  * Authorizes and makes a request to the Instagram API.
  */
 
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: 'keyavbuTLcnhJBwKt' }).base('appk3kBRjuom1ivqW');
 
-function main() {
-   var Airtable = require('airtable');
-   var base = new Airtable({ apiKey: 'keyavbuTLcnhJBwKt' }).base('appk3kBRjuom1ivqW');
-   base('days').create({
-      "date": "2016-10-23",
-      "media": 507,
-      "follows": 1032,
-      "followed_by": 6912
-      }, function(err, record) {
-            if (err) { console.log(err); return; }
-            console.log(record);
-         });     
-}
+//result = IG_getCounts();
+
+var igData = IG_getCounts();
+
+base('days').create( igData, function(err, record) {
+    if (err) { console.log(err); return; }
+    console.log(record);
+}); 	
 
 function IG_getCounts() {
    var ACCESS_TOKEN = '1347857108.058707f.f8701d6afb4941c498e977bba6fef51e';
-   var url = 'https://api.instagram.com/v1/users/self/?access_token=' + ACCESS_TOKEN;
-   var response = UrlFetchApp.fetch(url);         
-   var result = JSON.parse(response.getContentText());
- //  return result.data.counts;
+   Instagram = require('instagram-node-lib');
+   //Instagram.set('ACCESS_TOKEN', ACCESS_TOKEN)
+   var data = Instagram.users.self( {
+   	access_token: '1347857108.058707f.f8701d6afb4941c498e977bba6fef51e',
+	   complete: function(data){
+	       console.log(data);
+	     }
+	   });
+   return data   
 }
+   //var url = 'https://api.instagram.com/v1/users/self/?access_token=' + ACCESS_TOKEN;
+   //var response = UrlFetchApp.fetch(url);         
+   
+   
+   
+  // var result = JSON.parse(response.getContentText());
+   //return result.data.counts;
+
